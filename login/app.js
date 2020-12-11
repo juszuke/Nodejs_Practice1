350adbca44c28e240e538195c342cfbdbc63d44a
juszuke <yusuke.ino.yusuke@gmail.com>
2020-12-12 00:05:12 +0900

    ログイン画面と登録画面を作成。Blumaを導入。

--- login/app.js
+++ login/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var signInRouter = require('./routes/signIn');
+var signUpRouter = require('./routes/signUp');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/sign-in', signInRouter);
+app.use('/sign-up', signUpRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
